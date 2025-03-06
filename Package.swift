b816cbb6af18c01eb7fc08180f6629e34144a09e
Idan Schechter <idan8897@gmail.com>
2025-03-07 01:06:26 +0200

    Attempt at upgrading

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,9 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Smile",
-            dependencies: [],
-            path: "Sources"),
+            name: "Smile"),
         .testTarget(
             name: "SmileTests",
             dependencies: ["Smile"],
